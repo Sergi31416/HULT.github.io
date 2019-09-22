db8c5adbf909c6fd0f2fd1b853b38c171d8b030c
Sergi31416 <sergi31416@gmail.com>
2019-09-22 12:34:42 +0100

    sCROLL iNDICATROR

--- para.js
+++ para.js
@@ -31,3 +31,16 @@ $('.burger, .overlay').click(function(){
     
   
   });
+
+
+
+
+  // When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
